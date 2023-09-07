9451f136fcdf0d37c86e74f61ebbb4d16c299184
Joseph <josephopio11@gmail.com>
2023-09-07 19:54:31 +0800

    First Commit

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Todo;
+use Livewire\Component;
+use Livewire\Attributes\Rule;
+use Livewire\WithPagination;
+
+class TodoList extends Component
+{
+    use WithPagination;
+
+    #[Rule('required|min:3|max:255')]
+    public $name;
+
+    public $search;
+
+    public $editing_id;
+    public $new_name;
+
+    public function create()
+    {
+        // Validate
+        $binankani = $this->validateOnly('name');
+
+        Todo::create($binankani);
+
+        $this->reset('name');
+        session()->flash('success', 'Created!');
+
+        // ddd($this->name);
+    }
+
+    public function delete(Todo $todo)
+    {
+        $todo->delete();
+        session()->flash('success', 'Deleted!');
+    }
+
+    public function toggle(Todo $todo)
+    {
+        $todo->update(['completed' => !$todo->completed]);
+        session()->flash('updated'.$todo->id, 'Updated!');
+    }
+
+    public function edit(Todo $todo)
+    {
+        $this->editing_id = $todo->id;
+        $this->new_name = $todo->name;
+    }
+
+    public function render()
+    {
+        $todos = Todo::latest()->where('name','like',"%$this->search%")->paginate(5);
+        return view('livewire.todo-list',['todos' => $todos]);
+    }
+}
